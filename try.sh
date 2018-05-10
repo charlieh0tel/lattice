4e28fd57124dd7ae4a30a713be8cc9fc43a03df3
Christopher Hoover <charliehotel@google.com>
2018-05-10 03:51:42 +0000

    x

--- try.sh
+++ try.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
 set -o xtrace
+set -o errexit
 
+make
+
+# if compile with Wiring
+#
 # Wiring calls pin 37 GPIO 25.
 # gpio unexportall
 #./cross_program  /dev/i2c-1  0x40 25 ./gpio_toggle_impl1_original.bit
