0af6ff89a9b2fda7424dd415e60810e13cf5040d
Christopher Hoover <charliehotel@google.com>
2018-05-10 03:50:39 +0000

    x

---
+++ try.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -o xtrace
+
+# Wiring calls pin 37 GPIO 25.
+# gpio unexportall
+#./cross_program  /dev/i2c-1  0x40 25 ./gpio_toggle_impl1_original.bit
+
+# Kernel calls pin 37 GPIO 26.
+gpio export 26 output
+./cross_program  /dev/i2c-1  0x40 26 ./gpio_toggle_impl1_original.bit
